efafe40171ffa40ff1fbb9cb2da56eb747419393
SEVEN-S2 <arnoazsambane@gmail.com>
2026-09-20 12:10:16 +0200

    corrige cwd do pm2

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   apps: [{
     name: 'senna-bot',
     script: 'main.js',
-    cwd: '	/home/ubuntu/bots/senna',
+    cwd: '/home/ubuntu/bots/senna',
     node_args: '--max-old-space-size=256 --expose-gc',
     max_memory_restart: '300M',
     autorestart: true,
